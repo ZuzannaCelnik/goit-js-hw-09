94548003264e07c39574c17e15a0bc676ceaa54c
Zucela <101594376+ZuzannaCelnik@users.noreply.github.com>
2022-08-08 01:03:42 +0200

    task 1 updated

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const bodyColor = document.querySelector("body");
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
 
+let timeID = null;
+
+
+startBtn.addEventListener("click", () => {
+  timeID = setInterval(() => {
+    bodyColor.style.background = getRandomHexColor();
+    startBtn.disabled = true;}, 1000);
+stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timeID);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
