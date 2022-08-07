92fa4b0358f8078ef20f15f8f473bb382e12050b
ZuzannaCelnik <101594376+ZuzannaCelnik@users.noreply.github.com>
2022-08-07 23:04:05 +0000

    Deploying to gh-pages from @ ZuzannaCelnik/goit-js-hw-09@94548003264e07c39574c17e15a0bc676ceaa54c 🚀

---
+++ 01-color-switcher.9dcef4c0.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),a=null;e.addEventListener("click",(function(){a=setInterval((function(){t.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16)),e.disabled=!0}),1e3),n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(a),e.disabled=!1,n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.9dcef4c0.js.map
